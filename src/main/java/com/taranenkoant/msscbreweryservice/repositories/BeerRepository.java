79a84f3733f575e3bfc6167a274e8ca899d38e93
Антон Тараненко <taranenkoant@gmail.com>
2020-10-27 16:39:21 +0300

    adding beer repository. Closes #6

---
+++ src/main/java/com/taranenkoant/msscbreweryservice/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package com.taranenkoant.msscbreweryservice.repositories;
+
+import com.taranenkoant.msscbreweryservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
