9da369fc76b6ef55e4d3fe23eadcce0557727527
Антон Тараненко <taranenkoant@gmail.com>
2020-11-16 13:52:08 +0300

    adding error handler. Closes #

--- src/main/java/com/taranenkoant/msscbreweryservice/web/controller/BeerController.java
+++ src/main/java/com/taranenkoant/msscbreweryservice/web/controller/BeerController.java
@@ -3,6 +3,7 @@ package com.taranenkoant.msscbreweryservice.web.controller;
 import com.taranenkoant.msscbreweryservice.web.model.BeerDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -18,13 +19,13 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDTO beerDTO){
+    public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDTO beerDTO){
         //todo impl
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDTO beerDTO){
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody @Validated BeerDTO beerDTO){
         //todo impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
